005aa8b0c4b1cf9c730be42fa16850f0f2bd0ba5
justussoh <justus.soh@hotmail.com>
2020-03-07 22:19:23 -0800

    Added other structures

--- api/endpoints/chat.py
+++ api/endpoints/chat.py
@@ -6,19 +6,65 @@ class Chat:
         data = request.media
         query_result = data['queryResult']
         intent = query_result['intent']['displayName']
-        fulfillment_message = get_fulfillment_message(intent)
-        reply = {
-            "fulfillmentText": fulfillment_message,
-            "payload": {
-                "facebook": {
-                    "text": fulfillment_message
-                }
-            }
-        }
+        reply = get_fulfillment(intent)
         response.media = reply
 
 
+def get_fulfillment(intent):
+    response = get_fulfillment_message(intent)
+    if response['type'] == 'text':
+        return _simple_message_response(response['content'])
+    elif response['type'] == 'url':
+        return _url_message_response(response['content'])
+    return _simple_message_response("Sorry can you say that again!")
+
+
 def get_fulfillment_message(intent_name):
+    response = dict()
     if intent_name == "Virus Situation - Total Worldwide Cases":
-        return "There are currently {} cases worldwide".format(123) # replace 123 with getFromRedis(location, field): REFER TO REDIS FILE FOR BREAK DOWN OF AVAIL FIELDS
-    return "No query found"
+        response['type'] = "text"
+        response['content'] = "There are currently {} cases worldwide".format(123)
+    elif intent_name == "Best Practices":
+        response['type'] = "url"
+        response['content'] = {
+            'url': 'https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public',
+            'text': 'WHO recommends the following practices',
+            'title': 'Best Practices'
+        }
+    return response
+
+
+def _simple_message_response(text):
+    reply = {
+        "fulfillmentText": text,
+        "payload": {
+            "facebook": {
+                "text": text
+            }
+        }
+    }
+    return reply
+
+
+def _url_message_response(context):
+    reply = {'fulfillmentMessages': [{
+        'payload': {
+            "facebook": {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "button",
+                        "text": context['text'],
+                        "buttons": [{
+                            "type": "web_url",
+                            "url": context['url'],
+                            "title": context['title']
+                        }]
+                    }
+                }
+            }
+        }
+    }]
+    }
+    return reply
+
