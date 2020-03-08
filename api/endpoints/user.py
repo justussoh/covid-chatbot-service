82e46f560bc709ea3abf45ea7f445417639363b7
Pereira Yip <pereirayip@Pereiras-MacBook-Pro.local>
2020-03-08 01:44:46 -0800

    create user api

--- api/endpoints/user.py
+++ api/endpoints/user.py
@@ -3,6 +3,11 @@ from datetime import datetime
 
 import psycopg2 as psy
 
+try:
+    conn = psy.connect("dbname='covid' user='admin' host='35.239.162.61' password='12345678'")
+except:
+    print("Error connecting to database.")
+
 class User:
     # Return user based on user_id
     def on_get(self, req, resp):
@@ -11,6 +16,17 @@ class User:
 
     # Create user (TODO: Customize this as necessary to accommodate dialogflow)
     def on_post(self, req, resp):
-        req
+        data = req.media
+        session_id = data['session_id']
+        name = data['name']
+
+        query = "INSERT INTO users (session_id,name) VALUES ( '{}', '{}')".format(session_id, name)
+        
+
+        cur = conn.cursor()
+        cur.execute(query)
+        cur.close()
+        conn.commit()
+
         resp.status = falcon.HTTP_200
-        resp.media = ()
+        resp.media = ('User created.')
